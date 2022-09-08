66d4a93c722526d651ed2ac0c076f33429128536
Pheon <pio.lecki@gmail.com>
2022-09-08 23:24:59 +0200

    Test workflow with passing tests

--- src/test/kotlin/org/example/HelloTest.kt
+++ src/test/kotlin/org/example/HelloTest.kt
@@ -1,13 +1,8 @@
 package org.example
 
 import io.kotlintest.specs.ShouldSpec
-import kotlin.test.fail
 
 class HelloTest : ShouldSpec({
-
-    should("fail") {
-        fail("test failed on purpose")
-    }
 
     should("pass") {
         assert(true)
